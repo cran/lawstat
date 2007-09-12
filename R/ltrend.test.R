e671c12bae8a1571fad5412a7f568fcb67ef6da9
W. L. Wallace Hui <wlwhui@uwaterloo.ca>
2007-09-12 00:00:00 +0000

    version 2.0

--- R/ltrend.test.R
+++ R/ltrend.test.R
@@ -1,93 +1,61 @@
-"ltrend.test" <- 
-function(y, group, option=c("mean", "median", "trim.mean"), trim.alpha=1) 
- { 
-
-   option<-match.arg(option)
-   DNAME = deparse(substitute(y))
-
-
-##Strip NAs
-y<-na.omit(y)
-
-
-if ((option=="trim.mean")&(trim.alpha==1)) 
-{stop("trim.alpha value of 0 to 0.5 should be provided for the trim.mean option")}
-
-   group <- as.factor(group) # precautionary 
-
-
-### User can choose the trend test for variances based on classical Levene's Test, 
-### the trend test based on the robust Brown-Forsythe Levene-type test
-### or the Levene-type test with the group trimmed means  
-
-
-if(option=="mean")
+"ltrend.test"<-
+function (y, group, score=NULL, option = c("mean", "median", "trim.mean"), 
+    trim.alpha = 1) 
 {
 
-############## trend test with mean (Levene's version) ######################## 
-   means <- tapply(y, group, mean) 
-   resp.mean <- abs(y - means[group])
-   
-   mu<-mean(resp.mean)
-   
-   n<-tapply(y, group, length)
-   dbar<-sum(as.numeric(factor(levels(group)))*n)/sum(n)
-   z<-as.vector(resp.mean-mu)
-   d<-as.numeric(group)-dbar
- 
-   statistic=summary(lm(z~d))$coefficients[2,1]
-   METHOD="ltrend test based on classical Levene's procedure using the group means"
-   p.value=summary(lm(z~d))$coefficients[2,4]
- 
+    if(is.null(score)){score=group}
+
+    option <- match.arg(option)
+    DNAME = deparse(substitute(y))
+    y <- na.omit(y)
+    if ((option == "trim.mean") & (trim.alpha == 1)) {
+        stop("trim.alpha value of 0 to 0.5 should be provided for the trim.mean option")
+    }
+    gr<-score
+    group <- as.factor(group)
+    if (option == "mean") {
+        means <- tapply(y, group, mean)
+        resp.mean <- abs(y - means[group])
+        mu <- mean(resp.mean)
+        n <- tapply(y, group, length)
+        dbar <- sum(as.numeric(factor(levels(group))) * n)/sum(n)
+        z <- as.vector(resp.mean - mu)
+        d <- as.numeric(gr) - dbar
+        statistic = summary(lm(z ~ d))$coefficients[2, 1]
+        METHOD = "ltrend test based on classical Levene's procedure using the group means"
+        p.value = summary(lm(z ~ d))$coefficients[2, 4]
+    }
+    else if (option == "median") {
+        means <- tapply(y, group, median)
+        resp.mean <- abs(y - means[group])
+        mu <- mean(resp.mean)
+        n <- tapply(y, group, length)
+        dbar <- sum(as.numeric(factor(levels(group))) * n)/sum(n)
+        z <- as.vector(resp.mean - mu)
+        d <- as.numeric(gr) - dbar
+        statistic = summary(lm(z ~ d))$coefficients[2, 1]
+        METHOD = "ltrend test based on the modified Brown-Forsythe Levene-type procedure using the group medians"
+        p.value = summary(lm(z ~ d))$coefficients[2, 4]
+    }
+    else {
+        option = "trim.mean"
+        trimmed.mean <- function(y) mean(y, trim = trim.alpha)
+        means <- tapply(y, group, trimmed.mean)
+        resp.mean <- abs(y - means[group])
+        resp.mean <- abs(y - means[group])
+        mu <- mean(resp.mean)
+        n <- tapply(y, group, length)
+        dbar <- sum(as.numeric(factor(levels(group))) * n)/sum(n)
+        z <- as.vector(resp.mean - mu)
+        d <- as.numeric(gr) - dbar
+        statistic = summary(lm(z ~ d))$coefficients[2, 1]
+        METHOD = "ltrend test based on the modified Levene-type procedure using the group trimmed means"
+        p.value = summary(lm(z ~ d))$coefficients[2, 4]
+    }
+    STATISTIC = statistic
+    names(STATISTIC) = "Test Statistic"
+    structure(list(statistic = STATISTIC, p.value = p.value, 
+        method = METHOD, data.name = DNAME), class = "htest")
 }
 
 
-
-else if(option=="median")
-{
-
-############### trend test with median (BL version)###################
-   means <- tapply(y, group, median) 
-   resp.mean <- abs(y - means[group])
-   
-   mu<-mean(resp.mean)
-   
-   n<-tapply(y, group, length)
-   dbar<-sum(as.numeric(factor(levels(group)))*n)/sum(n)
-   z<-as.vector(resp.mean-mu)
-   d<-as.numeric(group)-dbar
- 
-   statistic=summary(lm(z~d))$coefficients[2,1]
-   METHOD="ltrend test based on the modified Brown-Forsythe Levene-type procedure using the group medians"
-   p.value=summary(lm(z~d))$coefficients[2,4]
-
-}
-
-
-else {option="trim.mean";
-
-############## trend test with trimmed mean (Joseph's version)##############
-   trimmed.mean<-function(y) mean(y, trim=trim.alpha)
-   means <- tapply(y, group, trimmed.mean) 
-   resp.mean <- abs(y - means[group])
-   resp.mean <- abs(y - means[group])
-   
-   mu<-mean(resp.mean)
-   
-   n<-tapply(y, group, length)
-   dbar<-sum(as.numeric(factor(levels(group)))*n)/sum(n)
-   z<-as.vector(resp.mean-mu)
-   d<-as.numeric(group)-dbar
-
-   statistic=summary(lm(z~d))$coefficients[2,1]
-   METHOD="ltrend test based on the modified Levene-type procedure using the group trimmed means"
-   p.value=summary(lm(z~d))$coefficients[2,4]
-
-} 
-  
-# Display Output #
-STATISTIC=statistic
-names(STATISTIC)="Test Statistic"
-
-structure(list(statistic = STATISTIC, p.value=p.value, method = METHOD, data.name = DNAME), class = "htest")
-   } 
--- R/rqq.R
+++ R/rqq.R
@@ -1,5 +1,5 @@
 "rqq" <-
-function(y, plot.it=TRUE, scale=c("MAD", "J", "classical"), 
+function(y, plot.it=TRUE, square.it=TRUE, scale=c("MAD", "J", "classical"), 
              location=c("median", "mean"), line.it=FALSE, 
         line.type=c("45 degrees", "QQ"), col.line=1, lwd=1, ...)
 {
@@ -37,20 +37,30 @@ else if((line.it=="TRUE")&(line.type=="45 degrees")){qql = ", 45 degrees line"}
 
 
 # User can select to plot QQ Line or 45 degrees Line #
+# User can also select Square Plot or not#
+
 if(line.it==TRUE)
 {
+  if(square.it==TRUE)
+    {
+    q<-qqnorm(y, xlim=c(min(y), max(y)), ylim=c(min(y), max(y)), main = paste(qqstd, location, qql), ...)
+    }
+    else {q<-qqnorm(y, main = paste(qqstd, location, qql), ...)}  
+
   if(line.type=="QQ")
-  {
-  q<-qqnorm(y, main = paste(qqstd, location, qql), ...)
-  qqline(y, datax = FALSE, col=col.line, lwd=lwd) 
-  }
-  else 
- {
-  q<-qqnorm(y, main = paste(qqstd, location, qql), ...)
-  abline(0, 1, col=col.line, lwd=lwd) 
- }
+    {
+    qqline(y, datax = FALSE, col=col.line, lwd=lwd) 
+    }
+    else {abline(0, 1, col=col.line, lwd=lwd)}
 }
-else 
-{q<-qqnorm(y, main = paste(qqstd, location),...)}
+
+else
+{
+  if(square.it==TRUE)
+    {
+    q<-qqnorm(y, xlim=c(min(y), max(y)), ylim=c(min(y), max(y)), main = paste(qqstd, location),...)
+    }
+    else {q<-qqnorm(y, main = paste(qqstd, location),...)}
+}
 
 }
