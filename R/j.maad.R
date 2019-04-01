3faacf36c1ef9d7128ace21c8b9847fa2fa42896
Vyacheslav Lyubchich <lyubchic@umces.edu>
2019-04-01 03:40:16 +0000

    version 3.3

--- R/j.maad.R
+++ R/j.maad.R
@@ -1,19 +1,37 @@
-`j.maad` <-
-function (x) 
+#' MAAD Robust Standard Deviation
+#' 
+#' Compute average absolute deviation from the sample median, 
+#' which is a consistent robust estimate of the population standard deviation 
+#' for normally distribution data \insertCite{Gastwirth_1982}{lawstat}. 
+#' \code{NA}s from the data are omitted.
+#'
+#'
+#' @param x a numeric vector of data values.
+#'
+#'
+#' @return Robust standard deviation.
+#' 
+#' @references
+#' \insertAllCited{}
+#' 
+#' @seealso \code{\link{cd}}, \code{\link{gini.index}}, \code{\link{rqq}}, 
+#' \code{\link{rjb.test}}, \code{\link{sj.test}}
+#' 
+#' @keywords homogeneity robust variability
+#' 
+#' @author Wallace Hui, Yulia R. Gel, Joseph L. Gastwirth, Weiwen Miao
+#' 
+#' @export
+#' @examples
+#' ## Sample 100 observations from the standard normal distribution
+#' x = rnorm(100)
+#' j.maad(x)
+#' 
+`j.maad` <- function(x) 
 {
-
-DNAME=deparse(substitute(x))
-
-## Strip NAs
-x<-na.omit(x)
-
-
-### Robust Standard Deviation J
-J<-sqrt(pi/2)*mean(abs(x-median(x))) 
-
-# return(J)
-
-paste("MAAD estimated J =", J, "for data", DNAME)
-
+    x <- na.omit(x)
+    ### Robust Standard Deviation J
+    J <- sqrt(pi/2)*mean(abs(x-median(x))) 
+    return(J)
 }
 
